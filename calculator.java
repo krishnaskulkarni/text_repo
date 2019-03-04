60031b44442f7d5070e5bcff6ecae04d7c9f7271
Kulkarni <krishna.a.kulkarni@capgemini.com>
2019-03-04 15:10:28 +0530

    calculator.java edited

--- calculator.java
+++ calculator.java
@@ -2,5 +2,6 @@ public class calculator{
 	public static void main(String []args){
 		System.out.println("hello world");
 		System.out.println("calculator java programm");
+		System.out.println("calculator java");
 	}
 }
