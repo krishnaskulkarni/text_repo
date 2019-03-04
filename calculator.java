0b0a1c79c850a6027fb20d02927646c7a46ac7c0
Kulkarni <krishna.a.kulkarni@capgemini.com>
2019-03-04 15:12:49 +0530

    Revert "calculator.java edited"
    This reverts commit 60031b44442f7d5070e5bcff6ecae04d7c9f7271.
    
    :wq

--- calculator.java
+++ calculator.java
@@ -2,6 +2,5 @@ public class calculator{
 	public static void main(String []args){
 		System.out.println("hello world");
 		System.out.println("calculator java programm");
-		System.out.println("calculator java");
 	}
 }
